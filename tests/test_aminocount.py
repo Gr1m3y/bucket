cc60fb5dd64319f140f569af53cce958e34cf7cd
Angus Hilts <angus.hilts@univie.ac.at>
2022-02-28 16:09:42 +0100

    Create test_aminocount.py

---
+++ tests/test_aminocount.py
@@ -0,0 +1,13 @@
+from aminocount import aminocount
+
+
+def test_counter(capfd):
+    aminocount.get_counts("tests/data/sample.faa")
+    out, err = capfd.readouterr()
+    results = {}
+    for line in out.splitlines():
+        aa = line.split("\t")[0].strip()
+        count = int(line.split("\t")[1])
+        results[aa] = count
+    assert(results["M"] == 37)
+    assert(results["W"] == 7)
