9c88e89eb99760cba0b7090ff17b8b8e2ef40d56
Angus Hilts <angus.hilts@univie.ac.at>
2022-02-28 16:08:46 +0100

    Update aminocount.py

--- aminocount/aminocount.py
+++ aminocount/aminocount.py
@@ -1,2 +1,22 @@
 import pyfastx
+import sys
+from collections import Counter
 
+
+def get_counts(faa_in):
+    """prints count of all amino acids in faa_in, in tsv format"""
+    all_proteins = ""
+    for name, seq in pyfastx.Fasta(faa_in, build_index=False):
+        seq = seq.strip()
+        all_proteins = f"{all_proteins}{seq}"
+    counts = Counter(all_proteins)
+    for aa in counts:
+        print(f"{aa}\t{counts[aa]}")
+
+
+def main():
+    get_counts(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
